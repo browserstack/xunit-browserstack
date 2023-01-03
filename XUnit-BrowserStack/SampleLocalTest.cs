2600fbdc1b52db4f87f094af3dfaefe557a4d1de
Sourav Kunda <50537259+07souravkunda@users.noreply.github.com>
2023-01-03 21:28:37 +0530

    update: refactor for sdk

---
+++ XUnit-BrowserStack/SampleLocalTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using OpenQA.Selenium.Remote;
+using System.Text.RegularExpressions;
+
+namespace XUnit_BrowserStack
+{
+    public class SampleLocalTest : IClassFixture<BaseFixture>
+    {
+
+        private readonly BaseFixture baseFixture;
+
+        public SampleLocalTest(BaseFixture baseFixture)
+        {
+            this.baseFixture = baseFixture;
+        }
+
+        [Fact]
+        [Trait("profile", "sample_local_test")]
+        public void Test()
+        {
+            RemoteWebDriver driver = baseFixture.GetDriver("chrome", "local");
+            driver.Navigate().GoToUrl("http://bs-local.com:45454/");
+            Assert.True(Regex.IsMatch(driver.Title, @"/BrowserStack Local/i", RegexOptions.IgnoreCase));
+        }
+    }
+}
